3eea6bc8668de02d2ee9aeab4ca88ecda528eebf
Tim <techwithtimm@gmail.com>
2024-12-16 21:35:11 +0400

    first commit

---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,39 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import PostCard from "../components/PostCard";
+import { FaArrowLeft } from "react-icons/fa";
+import "../styles/PostPage.css";
+
+const PostPage = () => {
+  const { postId } = useParams();
+  const navigate = useNavigate();
+  const post = useQuery(api.post.getPost, { id: postId });
+
+  if (!post) {
+    return (
+      <div className="post-page loading">
+        <div className="container">Loading...</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="post-page">
+      <div className="container">
+        <div className="page-header">
+          <div
+            onClick={() => navigate(-1)}
+            className="back-link"
+            style={{ cursor: "pointer" }}
+          >
+            <FaArrowLeft /> Back
+          </div>
+        </div>
+        <PostCard post={post} showSubreddit={true} expandedView={true} />
+      </div>
+    </div>
+  );
+};
+
+export default PostPage;
